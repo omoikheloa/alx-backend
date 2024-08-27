5a56d35dfe9addf7f8a4e6b46c3f6993a27b121c
omo <omoikheloa56@gmail.com>
2024-08-27 18:47:09 +0000

    edits

--- 0x02-i18n/5.app.py
+++ 0x02-i18n/5.app.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """A Basic Flask app with internationalization support.
 """
-import pytz
+from flask_babel import Babel
 from typing import Union, Dict
-from flask_babel import Babel, format_datetime
 from flask import Flask, render_template, request, g
 
 
@@ -30,9 +29,9 @@ users = {
 def get_user() -> Union[Dict, None]:
     """Retrieves a user based on a user id.
     """
-    login_id = request.args.get('login_as', '')
+    login_id = request.args.get('login_as')
     if login_id:
-        return users.get(int(login_id), None)
+        return users.get(int(login_id))
     return None
 
 
@@ -48,43 +47,18 @@ def before_request() -> None:
 def get_locale() -> str:
     """Retrieves the locale for a web page.
     """
-    queries = request.query_string.decode('utf-8').split('&')
-    query_table = dict(map(
-        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
-        queries,
-    ))
-    locale = query_table.get('locale', '')
+    locale = request.args.get('locale', '')
     if locale in app.config["LANGUAGES"]:
         return locale
-    user_details = getattr(g, 'user', None)
-    if user_details and user_details['locale'] in app.config["LANGUAGES"]:
-        return user_details['locale']
-    header_locale = request.headers.get('locale', '')
-    if header_locale in app.config["LANGUAGES"]:
-        return header_locale
-    return app.config['BABEL_DEFAULT_LOCALE']
-
-
-@babel.timezoneselector
-def get_timezone() -> str:
-    """Retrieves the timezone for a web page.
-    """
-    timezone = request.args.get('timezone', '').strip()
-    if not timezone and g.user:
-        timezone = g.user['timezone']
-    try:
-        return pytz.timezone(timezone).zone
-    except pytz.exceptions.UnknownTimeZoneError:
-        return app.config['BABEL_DEFAULT_TIMEZONE']
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/')
 def get_index() -> str:
     """The home/index page.
     """
-    g.time = format_datetime()
-    return render_template('index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
